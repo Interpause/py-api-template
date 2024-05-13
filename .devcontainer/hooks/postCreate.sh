299d085cb71887d6dd01884860ea6b8a1fe7c187
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-05-13 11:17:33 +0000

    fix: apt volume cache getting deleted

--- .devcontainer/hooks/postCreate.sh
+++ .devcontainer/hooks/postCreate.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+rm -f /etc/apt/apt.conf.d/docker-clean; \
+  echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' > /etc/apt/apt.conf.d/keep-cache
+
 poetry config virtualenvs.in-project true
 poetry config virtualenvs.prompt venv
 poetry install
