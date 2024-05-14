dd92a95cf0b9d1ccd0e3d65be304463ac91b7727
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-05-15 00:37:33 +0800

    fix: need sudo to edit docker-clean

--- .devcontainer/hooks/postCreate.sh
+++ .devcontainer/hooks/postCreate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-rm -f /etc/apt/apt.conf.d/docker-clean; \
-  echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' > /etc/apt/apt.conf.d/keep-cache
+sudo rm -f /etc/apt/apt.conf.d/docker-clean; \
+  echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' | sudo tee /etc/apt/apt.conf.d/keep-cache
 
 poetry config virtualenvs.in-project true
 poetry config virtualenvs.prompt venv
